0288d92865a79ea3cf639ec9256d3487d445aca2
Ryan Krawczyk <ryankrawczyk@ryans-macbook.bc.edu>
2018-10-12 00:17:36 -0400

    groundwork

--- src/MaxPQC.java
+++ src/MaxPQC.java
@@ -30,7 +30,29 @@ public class MaxPQC<T> implements MaxPQ<T extends Comparable<T>> {
         this.N = 0;
     }
 
-    public T evictMax() {}
+    public T evictMax() {
+        if (this.N == 0) { throw new NoSuchElementException("EMPTY QUEUE"); }
+        else if (this.N == 1) {
+            T item = this.first.info;
+            this.first = null;
+            this.last = null;
+        } else {
+            T item = this.first.info;
+            this.last.left = this.first.left;
+            this.last.right = this.first.right;
+            if (this.N % 2 == 0) { this.last.parent.right = null; }
+            else { this.last.parent.left = null; }
+            this.last.parent = null;
+            this.first = this.last;
+            if () {
+                // This is for the case of a single node remaining on the last level.
+            } else {
+
+            }
+        }
+        this.N--;
+        return item;
+    }
 
     public void insert(T key) {}
 
@@ -45,6 +67,8 @@ public class MaxPQC<T> implements MaxPQ<T extends Comparable<T>> {
     private void sink(Node key) {}
 
     private void swim(Node key) {}
+
+    private int level() { return Math.log(this.N) / Math.log(2); }
 
     public static void main(String[] args) {}
 
