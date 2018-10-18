56af152a412d0d401c5098abff7c32e41ade2bc5
Nick Hawk <hawkn@bc.edu>
2018-10-17 23:53:22 -0400

    FINAL

--- src/MaxPQC.java
+++ src/MaxPQC.java
@@ -11,12 +11,12 @@ public class MaxPQC<T extends Comparable<T>> implements MaxPQ<T> {
     private Node first, last;
     private int N;
 
-    private class Node {
+    private class Node { // Creating Node class
         T info;
         Node parent;
         Node left;
         Node right;
-        Node(T info, Node parent, Node left, Node right) {
+        Node(T info, Node parent, Node left, Node right) { // triply linked
             this.info = info;
             this.parent = parent;
             this.left = left;
@@ -24,7 +24,7 @@ public class MaxPQC<T extends Comparable<T>> implements MaxPQ<T> {
         }
     }
 
-    public MaxPQC() {
+    public MaxPQC() { // Creates priority queue and nulls pointers
         this.first = null;
         this.last = null;
         this.N = 0;
@@ -35,29 +35,40 @@ public class MaxPQC<T extends Comparable<T>> implements MaxPQ<T> {
     is evicted and all fields of MaxPQ are set to null. Otherwise, the info field of
     this.first is evicted and the info field of this.last is exchanged for it.
      */
-    public T evictMax() {
+    public T evictMax() { // returns root and swaps last leaf into roots position, then sinks
         T item;
-        if (this.isEmpty()) {
+        if (this.isEmpty()) { // empty queue, throws error
             throw new NoSuchElementException("EMPTY QUEUE");
-        }
-        else if (this.size() == 1) {
+        } else if (this.size() == 1) {
             item = this.first.info;
             this.first = null;
-            this.last = null;
         }
         else {
             item = this.first.info;
-            this.first.info = this.last.info;
-            this.setLast(this.first, --this.N);
+            this.first.info = nodeAt(this.size()).info;
+            if (this.size() % 2 == 0) { nodeAt(this.size() / 2).left = null; } // nulls out the parent's pointer to the child
+            else { nodeAt(this.size() / 2).right = null; }
             sink(this.first);
         }
+        this.N--;
         return item;
     }
 
-    public void insert(T key) {
-        this.setLast(this.first, ++this.N);
-        this.last.info = key;
-        swim(this.last);
+    public void insert(T key) { // creates node in the last position and swims the info field
+        if (this.isEmpty()) {
+          this.first = new Node(key, null, null, null);
+          this.N++;
+        } else {
+          this.N++;
+          Node parent = nodeAt(this.size() / 2);
+          if (this.N % 2 == 0) {
+            parent.left = new Node(key, parent, null, null); // inserts new Node at last position
+            swim(parent.left);
+          } else {
+            parent.right = new Node(key, parent, null, null);
+            swim(parent.right);
+          }
+        }
     }
 
     public boolean isEmpty() { return this.N == 0; }
@@ -66,7 +77,8 @@ public class MaxPQC<T extends Comparable<T>> implements MaxPQ<T> {
 
     public String toString() {
     StringBuilder ans = new StringBuilder();
-    ans.append("{" + first.info);
+    if (this.first == null) { ans.append("{"); }
+    else { ans.append("{" + first.info); }
     for (int i = 2; i <= this.size(); i++) {
         ans.append(", " + nodeAt(i).info);
     }
@@ -74,22 +86,22 @@ public class MaxPQC<T extends Comparable<T>> implements MaxPQ<T> {
     return ans.toString();
     }
 
-    private void exchange(Node key1, Node key2) {
+    private void exchange(Node key1, Node key2) { // exchanges info fields of two nodes
         T temp = key1.info;
         key1.info = key2.info;
         key2.info = temp;
     }
 
-    private void sink(Node key) {
-        while (!key.left.equals(null)) {
+    private void sink(Node key) { // compares parent to children and sinks parent if necessary
+        while (key.left != null) {
             Node greatest;
-            if ((!key.right.equals(null)) && (key.right.info > key.left.info)) {
+            if (key.right != null && key.right.info.compareTo(key.left.info) > 0) { // compare children to find largest one
                 greatest = key.right;
             }
             else {
                 greatest = key.left;
             }
-            if (greatest.info > key.info) {
+            if (greatest.info.compareTo(key.info) > 0) { // compare largest child to the sinker
                 exchange(greatest, key);
                 key = greatest;
             }
@@ -97,81 +109,62 @@ public class MaxPQC<T extends Comparable<T>> implements MaxPQ<T> {
         }
     }
 
-    private void swim(Node key) {
-        while (!key.parent.equals(null) && key.info > key.parent.info) {
+    private void swim(Node key) { // swims an info field up the tree if it is greater than the parent
+        while (key.parent != null && key.info.compareTo(key.parent.info) > 0) { // compare to the parent
             exchange(key, key.parent);
             key = key.parent;
         }
     }
 
-    private static int level(int N) {
-        int level = (int) Math.round(Math.log(N) / Math.log(2));
-        return level;
-    }
-
-    /* Recursively walks through the tree by continuously locating the parent node.
-    Required for the toString() method.
-     */
-    private Node nodeAt(int m) {
+    private Node nodeAt(int m) { // recursive walk through the tree to a node at a given position
         if (m == 1) return first;
         if (m % 2 == 0) return nodeAt(m / 2).left;
         else return nodeAt(m / 2).right;
     }
 
-    /* Algorithm for reassigning the this.last pointer. If it is passed this.size() - 1
-    in the case of evictMax(), this.last becomes the second to last leaf. If it is passed
-    this.size() + 1, this.last becomes a new leaf immediately following the last leaf.
-    The algorithm locates the Nth leaf by recursively passing back a left or right child
-    based on the number of leaves on the bottom level.
-     */
-    private T setLast(Node root, int N) {
-        int nodeCount = 0;
-        for (int i = 0; i < level(N); i++) {
-            nodeCount += Math.pow(2, i);
-        }
-        int bottomLeaves = N - nodeCount;
-
-        if (bottomLeaves == 1) {
-            if (root.left.equals(null)) { root.left = new Node(null, root, null, null); }
-            this.last = root.left;
-        } else if (bottomLeaves == 2) {
-            if (root.right.equals(null)) { root.right = new Node(null, root, null, null); }
-            this.last = root.right;
-        } else {
-            if (bottomLeaves <= Math.pow(2, level(N) - 1)) {
-                return setLast(root.left, N / 2);
-            } else {
-                return setLast(root.right, N / 2);
-            }
-        }
-        T fake = root.info;
-        return fake;
-    }
-
     public static void main(String[] args) {
         /* Unit Testing */
-        MaxPQ<Integer> testPQ = new MaxPQC<Integer>();
-        ArrayList<Integer> nums = new ArrayList<Integer>();
-        nums.add(3);
-        nums.add(114);
-        nums.add(56);
-        nums.add(94);
-        nums.add(169);
-        nums.add(8);
+        // Integers
+        MaxPQ<Integer> numTestPQ = new MaxPQC<Integer>();
+        //Inserting
+        numTestPQ.insert(3);
+        numTestPQ.insert(114);
+        numTestPQ.insert(56);
+        numTestPQ.insert(94);
+        numTestPQ.insert(169);
+        numTestPQ.insert(8);
 
-        // Inserting
         System.out.format("%n");
-        for (int i = 0; i < nums.size(); i++) {
-            testPQ.insert(nums.get(i));
-            System.out.format("After inserting %d: %s%n", nums.get(i), testPQ.toString());
-        }
-        System.out.format("Size of priority queue: %d%n%n", testPQ.size());
+        System.out.format("Priority queue after all inserts: %s%n", numTestPQ.toString());
+        System.out.format("Size of priority queue: %d%n%n", numTestPQ.size());
 
         // Evicting
-        for (int j = 0; j < nums.size(); j++) {
-            System.out.format("After evicting %d: %s%n", testPQ.evictMax(), testPQ.toString());
+        int roof = numTestPQ.size();
+        for (int i = 0; i < roof; i++) {
+            System.out.format("After evicting %d: %s%n", numTestPQ.evictMax(), numTestPQ.toString());
         }
-        System.out.format("Size of priority queue: %d%n%n", testPQ.size());
+        System.out.format("Size of priority queue: %d%n%n", numTestPQ.size());
+
+        // Strings
+        MaxPQ<String> stringTestPQ = new MaxPQC<String>();
+        // Inserting
+        stringTestPQ.insert("D");
+        stringTestPQ.insert("X");
+        stringTestPQ.insert("A");
+        stringTestPQ.insert("J");
+        stringTestPQ.insert("N");
+        stringTestPQ.insert("R");
+
+        System.out.format("%n");
+        System.out.format("Priority queue after all inserts: %s%n", stringTestPQ.toString());
+        System.out.format("Size of priority queue: %d%n%n", stringTestPQ.size());
+
+        // Evicting
+        roof = stringTestPQ.size();
+        for (int j = 0; j < roof; j++) {
+            System.out.format("After evicting %s: %s%n", stringTestPQ.evictMax(), stringTestPQ.toString());
+        }
+        System.out.format("Size of priority queue: %d%n%n", stringTestPQ.size());
     }
 
 }
